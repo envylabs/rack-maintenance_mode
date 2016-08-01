608a34428ef80c128bfeefea4e06ad3682b5bc9d
Olivier Lacan <hi@olivierlacan.com>
2016-07-31 22:47:57 -0400

    Rack 2.0 deprecated string references
    DEPRECATION WARNING: Passing strings or symbols to the middleware builder is deprecated, please change
    them to actual class references.  For example:
    
      "Rack::MaintenanceMode::Middleware" => Rack::MaintenanceMode::Middleware
    
    I think this is fairly backward-compatible assuming that Rack::MaintenanceMode::Middleware is loaded first.

--- lib/rack/maintenance_mode/railtie.rb
+++ lib/rack/maintenance_mode/railtie.rb
@@ -15,7 +15,7 @@ module Rack
       config.maintenance_mode = ActiveSupport::OrderedOptions.new
 
       initializer 'rack-maintenance_mode.insert_middleware' do |app|
-        app.config.middleware.use 'Rack::MaintenanceMode::Middleware', config.maintenance_mode
+        app.config.middleware.use Rack::MaintenanceMode::Middleware, config.maintenance_mode
       end
     end
   end
